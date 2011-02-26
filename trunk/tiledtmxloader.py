2960e71a64174a6e754bd1673fc39f8267608d5d
dr0iddr0id@gmail.com <dr0iddr0id@gmail.com@71ef771c-53ce-75a1-03fa-9e49aeb025ba>
2011-02-26 22:04:31 +0000

    version scheme documentation

--- trunk/tiledtmxloader.py
+++ trunk/tiledtmxloader.py
@@ -8,6 +8,26 @@ It loads the \*.tmx files produced by Tiled.
 
 
 """
+
+# Versioning scheme based on: http://en.wikipedia.org/wiki/Versioning#Designating_development_stage
+#
+#   +-- api change, probably incompatible with older versions
+#   |     +-- enhancements but no api change
+#   |     |
+# major.minor[.build[.revision]]
+#                |
+#                +-|* 0 for alpha (status)
+#                  |* 1 for beta (status)
+#                  |* 2 for release candidate
+#                  |* 3 for (public) release
+# 
+# For instance:
+#     * 1.2.0.1 instead of 1.2-a
+#     * 1.2.1.2 instead of 1.2-b2 (beta with some bug fixes)
+#     * 1.2.2.3 instead of 1.2-rc (release candidate)
+#     * 1.2.3.0 instead of 1.2-r (commercial distribution)
+#     * 1.2.3.5 instead of 1.2-r5 (commercial distribution with many bug fixes)
+
 __revision__ = "$Rev$"
 __version__ = "2.3.1." + __revision__[6:-2]
 __revision__ = u'$Id: tiledtmxloader.py 13 2011-02-22 19:29:13Z dr0iddr0id@gmail.com $'
@@ -948,7 +968,6 @@ class RendererPygame(object):
             self.source_rect = source_rect
             self.flags = flags
 
-    # TODO: methods: add_sprite/remove_sprite
     # TODO: collapse/uncollapse
     # TODO: do calculations of set_camera_position per layer
     def __init__(self, world_map):
