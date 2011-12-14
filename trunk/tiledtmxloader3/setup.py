74dfca8a6239be63b68e69f09e2baa9d0b4f2ac3
dr0iddr0id@gmail.com <dr0iddr0id@gmail.com@71ef771c-53ce-75a1-03fa-9e49aeb025ba>
2011-12-14 21:36:17 +0000

    fixed setup.py, now its installed correctly

--- trunk/tiledtmxloader3/setup.py
+++ trunk/tiledtmxloader3/setup.py
@@ -3,29 +3,29 @@
 from distutils.core import setup
 import sys
 
-sys.path.append('tiledtmxloader')
+sys.path.insert(0, 'tiledtmxloader')
 import tiledtmxloader
 
 
-setup(name='tiledtmxloader',
-      version=tiledtmxloader.__version__,
-      author='DR0ID',
-      author_email='dr0iddr0id@gmail.com',
-      maintainer='DR0ID',
-      url='https://code.google.com/p/pytmxloader/',
-      download_url='https://code.google.com/p/pytmxloader/downloads/list',
-      description='',
-      long_description=tiledtmxloader.tmxreader.__doc__,
-      package_dir={'': 'tiledtmxloader'},
-      py_modules=['tmxreader', '__init__', 'helperspygame', 'helperspyglet'],
-      provides=['tiledtmxloader'],
-      keywords='pygame tiled mapeditor game map',
-      license='New BSD License',
-      classifiers=['Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Developers',
-                   'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2',
-                   'License :: OSI Approved :: GNU General Public License Version 2',
-                  ],
+setup(
+    name='tiledtmxloader',
+    version=tiledtmxloader.__version__,
+    author='DR0ID',
+    author_email='dr0iddr0id@gmail.com',
+    maintainer='DR0ID',
+    url='https://code.google.com/p/pytmxloader/',
+    download_url='https://code.google.com/p/pytmxloader/downloads/list',
+    description='',
+    long_description=tiledtmxloader.tmxreader.__doc__,
+    package_dir={'tiledtmxloader': 'tiledtmxloader'},
+    packages=['tiledtmxloader'],
+    keywords='pygame tiled mapeditor game map',
+    license='New BSD License',
+    classifiers=['Development Status :: 5 - Production/Stable',
+               'Intended Audience :: Developers',
+               'Natural Language :: English',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 2',
+               'License :: OSI Approved :: New BSD License',
+                ],
      )
